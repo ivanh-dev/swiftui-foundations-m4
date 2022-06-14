66287d8f7771d5268795046ed9b8669c77e87fac
Ivan Hirsinger <ivan.hirsinger@gmail.com>
2022-06-14 12:50:29 +0200

    - add category tab - add button to reset filters

---
+++ Recipe List App/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Recipe List App
+//
+//  Created by Ivan Hirsinger on 14.06.2022..
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    static var featuredTab = 0
+    static var categoriesTab = 1
+    static var listTab = 2
+    
+    static var defaultListFiler = "All Recipes"
+}
--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -11,32 +11,69 @@ struct RecipeListView: View {
     
     @EnvironmentObject var model:RecipeModel
     
+    private var title: String {
+        if model.selectedCategory == nil || model.selectedCategory == Constants.defaultListFiler {
+            return "All Recipes"
+        }
+        else {
+            return model.selectedCategory!
+        }
+    }
+    
+    @State private var showsResetFilter = false
+    
     var body: some View {
         
         NavigationView {
             
             VStack(alignment: .leading) {
-                Text("All Recipes")
-                    .bold()
-                    .padding(.top, 40)
-                    .font(.largeTitle)
+                HStack {
+                    Text(title)
+                        .bold()
+                        .padding(.top, 40)
+                        .font(.largeTitle)
+                    
+                    Spacer()
+                    
+                    if model.selectedCategory != nil {
+                        Button  {
+                            model.selectedCategory = nil
+                        } label: {
+                            HStack {
+                                Text("Reset Filter")
+                                    .foregroundColor(.black)
+                                Image(systemName: "arrow.clockwise.circle")
+                                
+                            }
+                            .padding(.top, 40)
+                            .padding(.trailing)
+                        }
+                    }
+                    
+                }
                 
                 ScrollView {
                     
                     LazyVStack(alignment: .leading) {
                         ForEach(model.recipes) { r in
-                            NavigationLink(
-                                destination: RecipeDetailView(recipe: r)) {
-                                HStack(spacing: 20.0) {
-                                    Image(r.image)
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 50, height: 50, alignment: .center)
-                                        .clipped()
-                                        .cornerRadius(5)
-                                    Text(r.name)
-                                        .foregroundColor(.black)
-                                }
+                            
+                            if model.selectedCategory == nil ||
+                                model.selectedCategory == Constants.defaultListFiler ||
+                                model.selectedCategory != nil && r.category == model.selectedCategory {
+                                
+                                NavigationLink(
+                                    destination: RecipeDetailView(recipe: r)) {
+                                        HStack(spacing: 20.0) {
+                                            Image(r.image)
+                                                .resizable()
+                                                .scaledToFill()
+                                                .frame(width: 50, height: 50, alignment: .center)
+                                                .clipped()
+                                                .cornerRadius(5)
+                                            Text(r.name)
+                                                .foregroundColor(.black)
+                                        }
+                                    }
                             }
                         }
                     }
